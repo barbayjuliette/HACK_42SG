d5c698cbf611edc715202b0fcf4ac4e53b9c4992
Liew Yun Xuan <liewyxn@gmail.com>
2024-09-18 20:59:53 +0900

    add some responsiveness and hover transitions

--- hack/static/js/main.js
+++ hack/static/js/main.js
@@ -1,1 +1,22 @@
 console.log("This is JS from my About page")
+
+document.addEventListener('DOMContentLoaded', function()
+{
+	const topnavLinks = document.getElementById('topnavLinks');
+	const menuIcon = document.getElementById('menuIcon');
+
+	menuIcon.addEventListener('click', function()
+	{
+		event.stopPropagation();
+		topnavLinks.classList.toggle('show');
+	});
+
+	// Close the menu when clicking outside
+	document.addEventListener('click', function(event)
+	{
+		if (!topnavLinks.contains(event.target) && !menuIcon.contains(event.target))
+		{
+			topnavLinks.classList.remove('show');
+		}
+	});
+});
