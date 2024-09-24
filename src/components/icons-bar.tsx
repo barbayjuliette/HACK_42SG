721e8a8145c453a1064088f6db3d23be186c9b34
Roby Tanama <rtanama@c2r2s11.42singapore.sg>
2024-09-24 16:37:04 +0800

    feat: add iconSelected

--- src/components/icons-bar.tsx
+++ src/components/icons-bar.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { cn } from "@/lib/utils";
-import { FC, ReactElement } from "react";
+import { Filter } from "lucide-react";
+import { FC, ReactElement, useState } from "react";
 
 interface IconProps {
   icon: JSX.Element;
   title: string;
   selected?: boolean;
+  onClick?: VoidFunction;
 }
 
 interface IconsBarProps {
@@ -12,13 +15,14 @@ interface IconsBarProps {
 }
 
 const Icon: FC<IconProps> = (props) => {
-  const { icon, title, selected } = props;
+  const { icon, title, selected, onClick } = props;
   return (
     <div
       className={cn(
         "flex flex-col gap-2 items-center justify-center",
         selected ? "text-panda" : ""
       )}
+      onClick={onClick}
     >
       {icon}
       <p className="font-semibold text-sm">{title}</p>
@@ -28,10 +32,21 @@ const Icon: FC<IconProps> = (props) => {
 
 const IconsBar: FC<IconsBarProps> = (props) => {
   const { icons } = props;
+  const [selected, setSelected] = useState<IconProps | undefined>(
+    icons.find((icon) => icon.selected)
+  );
+
+  const onClick = (icon: IconProps) => {
+    setSelected(icon);
+  };
   return (
     <div className="flex gap-8 items-center justify-center">
       {icons.map((icon) => (
-        <Icon {...icon} />
+        <Icon
+          {...icon}
+          selected={selected?.title === icon.title}
+          onClick={() => onClick(icon)}
+        />
       ))}
     </div>
   );
