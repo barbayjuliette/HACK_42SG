ec9f696122dc253ce52cb746d1d57a08ad1a1611
bhowe <67160296+itben-h@users.noreply.github.com>
2024-09-25 01:12:22 +0900

    fix for smaller screens

--- src/components/gather-button.tsx
+++ src/components/gather-button.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import Timer from "./timer"; // Import Timer component
 
 interface GatherButtonProps {
   href: string;
@@ -14,10 +15,11 @@ const GatherButton: FC<GatherButtonProps> = (props) => {
     <Link href="/gather" className={cn("flex", className)}>
       <Button variant="default" size="lg" className="rounded-t-xl rounded-b-none bg-panda text-white flex h-auto px-8 py-8 items-center justify-start text-start w-full">
         <div className="flex flex-col flex-grow gap-2">
-          <p className="font-semibold text-lg">PandaGather Available Nearby</p>
-		  <p>There are 69 PandaGathers currently</p>
-		  <p>available near you!</p>
+          <p className="font-semibold text-lg">pandagather Available Nearby</p>
+          <p>There are 19 pandagathers currently</p>
+          <p>available near you!</p>
         </div>
+        <Timer /> {/* Add Timer component to the right */}
       </Button>
     </Link>
   );
