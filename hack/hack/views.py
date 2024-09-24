6980890353f45eccfb039530b8f170d3adf2131d
Juliette Barbay <barbayjuliette95@gmail.com>
2024-09-24 12:47:20 +0900

    removed everything

--- hack/hack/views.py
+++
@@ -1,8 +0,0 @@
-# from django.http import HttpResponse
-from django.shortcuts import render
-
-def homepage(request):
-	return render(request, 'home.html')
-
-def about(request):
-	return render(request, 'about.html')
