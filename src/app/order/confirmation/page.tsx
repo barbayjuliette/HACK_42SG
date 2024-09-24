f8fbd1f7a37e93cbbf055f49a37d91fe038da96c
Roby Tanama <rtanama@c2r2s11.42singapore.sg>
2024-09-24 21:41:37 +0800

    fix: update confirmation page look

--- src/app/order/confirmation/page.tsx
+++ src/app/order/confirmation/page.tsx
@@ -1,36 +1,54 @@
 import ExitButton from "@/components/exit-button";
 import Timer from "@/components/timer";
+import TotalButton from "@/components/total-button";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { restaurantName, userLocation } from "@/constants/general";
-import { ArrowRightCircle } from "lucide-react";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { ArrowRight, ArrowRightCircle, Bike, CreditCard } from "lucide-react";
 import Link from "next/link";
 
 export default function OrderConfirmationPage() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex-col gap-6 px-12 bg-panda_bg">
+    <div className="flex flex-col gap-8 bg-panda_bg w-full items-center">
       <ExitButton />
-      <Timer className="h-[180px] w-[180px] text-5xl border-8" />
-      <Button className="w-full h-[121px] bg-panda_light text-white">
-        SAMPLE TEXT
-      </Button>
-      <p className="font-bold text-lg">{restaurantName}</p>
-      <div className="flex flex-col gap-2 items-center justify-center">
-        <p className="font-bold text-lg">to</p>
-        <p className="font-semibold">{userLocation}</p>
-      </div>
-      <div className="rounded-lg flex flex-col p-4 bg-white border-2 text-sm w-full items-center">
-        <div className="flex gap-2 items-center">
-          <p>Delivery 10 - 25 min</p>
-          <p className="font-bold">|</p>
-          <p className="font-bold">3 items max</p>
+      <div className="p-12 bg-restaurant bg-cover bg-opacity-20 w-full items-center justify-center shadow-lg">
+        <div className="p-4 flex flex-col gap-6 rounded-lg bg-panda_bg items-center justify-center opacity-90">
+          <Timer className="h-[180px] w-[180px] text-5xl border-8" />
+          <p className="font-bold text-lg">{restaurantName}</p>
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <p className="font-bold text-lg">to</p>
+            <p className="font-semibold">{userLocation}</p>
+          </div>
         </div>
-        <p className="font-bold text-lg">You will save</p>
-        <p className="font-bold text-lg text-panda">69 km</p>
-        <p className="font-bold text-lg text-panda">Delivery $8.00</p>
+      </div>
+      <div className="flex gap-2 items-center justify-center bg-white py-3 w-[340px] mx-8 rounded-xl border">
+        <InfoCircledIcon />
+        <p>Delivery 10 - 25 min</p>
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-center bg-white py-4 w-[340px] mx-8 rounded-xl border">
+        <p className="font-semibold">YOU SAVE</p>
+        <div className="flex gap-8 items-center justify-center text-center">
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <Bike />
+            <div className="flex flex-col items-center justify-center">
+              <p>32 km</p>
+              <p>travelled</p>
+            </div>
+          </div>
+          <Separator orientation="vertical" />
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <CreditCard />
+            <div className="flex flex-col items-center justify-center">
+              <p>$32.00</p>
+              <p>on delivery</p>
+            </div>
+          </div>
+        </div>
       </div>
       <Link href="/order/selection">
-        <Button variant="ghost" size="icon">
-          <ArrowRightCircle height={40} width={40} />
+        <Button className="h-[86px] w-[340px] flex items-center justify-center rounded-xl bg-panda">
+          <ArrowRight size={48} />
         </Button>
       </Link>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,9 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        restaurant: "url('/restaurant-blur.png')",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
